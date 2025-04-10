942fc6c560e81e4ded6d2a8ef73aee8225cd29a9
David Djuretanovic <djues3@gmail.com>
2025-04-10 17:39:32 +0200

    Revert "pocetak firebase implementacije"
    This reverts commit e50ae23e94359bbcff017393f277b59712790957.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,11 @@
 import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
-import NotificationHandler from "./components/NotificationHandler";
 
 export default function RootLayout() {
   return (
     
     <>
     <StatusBar style="light"/>
-    <NotificationHandler />
       <Stack>
         <Stack.Screen name="index"
         options={{
